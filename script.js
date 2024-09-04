56746f15e30a83537aec807efc4943574c628638
Arthur <arthur91021@gmail.com>
2024-09-04 14:11:59 -0700

    added PsuedoCode

--- script.js
+++ script.js
@@ -16,8 +16,21 @@ const displayInput = formData.addEventListener("submit", function (e) {
   listData.appendChild(li);
 });
 
-// Delete Item
+/**  Delete Item
+    1) render delete btn to li items
+    2) addEventListener to button that will...
+    3) removeChild()
+    */
 
-// Cross Off Item
+/**Cross Off Item
+    1) addClassName to li 
+    2) add CSS styling 'crossing off effect' 
+      a) ul li.(className here): text-decoration-line-through
+    3) toggleClassNames (on click)
+    4) magic
 
-// Add To Local Storage
+*/
+/**  Add To Local Storage
+    1) create function with localstorage('data', innerHTML)
+    2) render local storage function
+*/
