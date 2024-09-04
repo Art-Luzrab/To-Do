7cd1482c3f77b0a35196188c0a74f0059d04d397
Arthur <arthur91021@gmail.com>
2024-09-04 13:30:41 -0700

    created addItem function

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 //grab form data from user inputs
 const formData = document.querySelector(".formContainer");
 const listData = document.querySelector(".listContainer");
+const userInput = document.getElementById("userInput").value;
 
-const displayInput = formData.addEventListener("submit", function (e) {
-  e.preventDefault();
+// const displayInput = formData.addEventListener("submit", function (e) {
+//   e.preventDefault();
 
-  const userInput = document.getElementById("userInput").value;
-  document.getElementById("userInput").value = "";
-  console.log(userInput);
-});
+//   document.getElementById("userInput").value = "";
+//   console.log(userInput);
+// });
 
 //rendering html
 let html = "<li>This is a task. <button>Delete</button></li>";
 
 listData.insertAdjacentHTML("afterbegin", html);
+
+const addItem = function () {
+  let li = document.createElement("li");
+
+  li.innerHTML = userInput.value;
+  listData.appendChild(li);
+};
