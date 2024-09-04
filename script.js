9a6dd7467222a673f95515db50b40bcbbf9dc8cd
Arthur <arthur91021@gmail.com>
2024-09-04 13:49:04 -0700

    you can now add tasks successfully

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 //grab form data from user inputs
 const formData = document.querySelector(".formContainer");
 const listData = document.querySelector(".listContainer");
-const userInput = document.getElementById("userInput").value;
 
-// const displayInput = formData.addEventListener("submit", function (e) {
-//   e.preventDefault();
+const displayInput = formData.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-//   document.getElementById("userInput").value = "";
-//   console.log(userInput);
-// });
+  const userInput = document.getElementById("userInput").value;
+  document.getElementById("userInput").value = "";
+  console.log(userInput);
 
-//rendering html
-let html = "<li>This is a task. <button>Delete</button></li>";
-
-listData.insertAdjacentHTML("afterbegin", html);
-
-const addItem = function () {
   let li = document.createElement("li");
 
-  li.innerHTML = userInput.value;
+  li.innerHTML = userInput;
   listData.appendChild(li);
-};
+});
